c2f2291ce3bb6b1b0361b7fa1c06781b36e4915e
Robert Gravina <robertg@reallyenglish.com>
2013-06-14 18:51:25 +0900

    Redmine 2.x fixes - Added routes.rb. Fixed default watchers admin screen.
    New issue hooks still need to be added.

---
+++ config/routes.rb
@@ -0,0 +1,2 @@
+get 'default_watchers', :to => 'default_watchers#index'
+post 'default_watchers', :to => 'default_watchers#update'
--- init.rb
+++ init.rb
@@ -2,9 +2,8 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_default_watchers do
   name 'Redmine Default Watchers plugin'
-  author 'Masatomo Nakano'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-
+  author 'Masatomo Nakano, Robert Gravina'
+  description 'Redmine plugin allowing users to manage a default watchers list. These watchers are checked by default when creating a new issue.'
+  version '0.1.0'
   menu :top_menu, :default_watchers, {:controller => :default_watchers, :action => :index}, :caption => 'Watchers'
 end
